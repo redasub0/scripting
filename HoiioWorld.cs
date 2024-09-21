d68dfd68dea53eb6a8543f2a6f90aa1a17b3d727
redasub0 <redaalzain85@gmail.com>
2024-09-21 11:45:38 +0300

    Add files via upload
    unity c# class 1

---
+++ HoiioWorld.cs
@@ -0,0 +1,19 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class holloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Output to the console greetings to the  world
+        print("hello, world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
